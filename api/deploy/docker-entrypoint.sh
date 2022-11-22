8102eeb37d126cf2ca2e4e246d76e996f5ddb475
Maurice Guikema <mguikema@gmail.com>
2022-11-22 15:15:42 +0100

    prepare for live

--- api/deploy/docker-entrypoint.sh
+++ api/deploy/docker-entrypoint.sh
@@ -3,12 +3,6 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x
 
-until PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST_OVERRIDE -U $DATABASE_USER -d $DATABASE_NAME -c '\q'; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-echo "Postgres is up!"
-
 echo Collecting static files
 python manage.py collectstatic --no-input
 
