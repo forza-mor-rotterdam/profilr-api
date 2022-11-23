37f3c45e8ec65cab2b5b1ccfc2915792df20ee09
Maurice Guikema <mguikema@gmail.com>
2022-11-23 09:19:42 +0100

    allowed hosts

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,11 +1,21 @@
-from django.http import HttpResponse
-from django.urls import path
+from apps.categories.viewsets import CategoryViewSet, SubCategoryViewSet
+from apps.incidents.viewsets import IncidentViewSet
+from apps.locations.viewsets import BuurtViewSet, WijkViewSet
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-
-def http_response(request):
-    return HttpResponse("<h1>Hello</h1>")
-
+router = DefaultRouter()
+router.register(r"incidents", IncidentViewSet, basename="incidents")
+router.register(r"wijken", WijkViewSet, basename="wijken")
+router.register(r"buurten", BuurtViewSet, basename="buurten")
+router.register(r"categories", CategoryViewSet, basename="categories")
+router.register(r"sub-categories", SubCategoryViewSet, basename="sub-categories")
 
 urlpatterns = [
-    path("hello", http_response, name="http_response"),
+    # Used to determine API health when deploying
+    path("status/", include("apps.health.urls")),
+    # The Django admin
+    path("admin/", admin.site.urls),
+    path("v1/", include((router.urls, "app"), namespace="v1")),
 ]
