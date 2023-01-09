bc0e939e0fdfbc3d71cc12e99a87ed5e2d30472e
Maurice Guikema <mguikema@gmail.com>
2023-01-09 19:35:04 +0100

    change profile user removed

--- api/apps/users/models.py
+++ api/apps/users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils.translation import gettext as _
 
 User = get_user_model()
 
@@ -13,17 +12,17 @@ class Profile(CreatedUpdatedModel):
     The profile model for a user
     """
 
-    user = models.OneToOneField(
-        to=User,
-        related_name="profile",
-        verbose_name=_("profile"),
-        on_delete=models.CASCADE,
-    )
+    # user = models.OneToOneField(
+    #     to=User,
+    #     related_name="profile",
+    #     verbose_name=_("profile"),
+    #     on_delete=models.CASCADE,
+    # )
 
     filters = models.JSONField(default=dict)
 
     def __str__(self):
-        return self.user.username
+        return f"Profile with id: {self.id}"
 
 
 @receiver(post_save, sender=User)
